a2734a7ab84bed17f44e7128b721b12856fce994
gaetano <guysblues@gmail.com>
2019-11-19 10:06:37 -0800

    Fixed NAN issues in data loading.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 # SARC directry paths
-SARC_DATA = os.path.expanduser('~/DATA/SARC/')
+# SARC_DATA = os.path.expanduser('~/DATA/SARC/')
+SARC_DATA = 'DATA/'
 SARC_MAIN = SARC_DATA + 'main/'
 SARC_POL = SARC_DATA + 'pol/'
